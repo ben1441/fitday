62c0787ae3060345c2a1ecdd2334b791f1f65f32
Manovah <manovah04@gmail.com>
2025-07-13 14:16:24 +0000

    gap-6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Dashboard() {
   }
 
   const MainContent = () => (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-24 lg:gap-8">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8">
       <div className="lg:col-span-2 space-y-auto">
         <DailyWorkout
           date={selectedDate}
